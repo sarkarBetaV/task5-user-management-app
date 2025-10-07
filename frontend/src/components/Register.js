fe561937dcd5cadf2af169828cae2f1bdf896328
Shipon Sarkar <ship1on2sarkar@gmail.com>
2025-10-07 10:13:37 +0600

    more .js extensions

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/AuthContext.js";
 
 const Register = () => {
   const [formData, setFormData] = useState({
